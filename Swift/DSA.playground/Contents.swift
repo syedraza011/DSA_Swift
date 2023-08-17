0ca8ea0fc5dd4d7100123496b2b0acb7236d5afb
Syed Raza <syed.raza011@gmail.com>
2023-08-17 15:25:03 -0400

    print & Separate number from word

--- Swift/DSA.playground/Contents.swift
+++ Swift/DSA.playground/Contents.swift
@@ -2,14 +2,20 @@ import UIKit
 
 var greeting = "Hello, playground"
 // "Hello5"
-//access only number
+
 //access 'o' or any character
-//sort these
+
 //remove number from it
 //remove chars from it
 //use of array
 let str: String = "Hello5"
-
-for i in 0..< str.count {
-    print(str[i])
+//access only number
+for (index, char) in str.enumerated() {
+    if char.isNumber
+    {
+        print("\(index), and \(char)")
+    }else {
+        print("char is ->", char)
+    }
 }
+////sort these
