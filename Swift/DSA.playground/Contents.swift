82290b7047f1646dcc36fd57115e1bad07cb6bbb
Syed Raza <syed.raza011@gmail.com>
2023-08-17 15:17:08 -0400

    breaking down questions

--- Swift/DSA.playground/Contents.swift
+++ Swift/DSA.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var greeting = "Hello, playground"
+// "Hello5"
+//access only number
+//access 'o' or any character
+//sort these
+//remove number from it
+//remove chars from it
+//use of array
+let str: String = "Hello5"
+
+for i in 0..< str.count {
+    print(str[i])
+}
