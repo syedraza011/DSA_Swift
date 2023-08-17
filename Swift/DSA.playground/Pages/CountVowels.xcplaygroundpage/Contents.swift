238ae7dbe08f0f5c7615266b91b5fad740c050b0
Syed Raza <syed.raza011@gmail.com>
2023-08-17 19:43:04 -0400

    updated 7 added a program where we replace vowels with a space " "

--- Swift/DSA.playground/Pages/CountVowels.xcplaygroundpage/Contents.swift
+++ Swift/DSA.playground/Pages/CountVowels.xcplaygroundpage/Contents.swift
@@ -25,4 +25,23 @@ func isVowel(char: Character)-> Bool{
             return vowels.contains(char)
         }
     
-// 
+//  replace vowels with " "
+let Str = "HelloiamRaza"
+let newArr = Str.split(separator: "")
+print("New Array: \(newArr)")
+var result = ""
+for singleChar in Str {
+    if ISVowel(char: singleChar) {
+        result += " "
+    }else {
+        result += String(singleChar)
+    }
+}
+let finalResult = String(result)
+print("new Replaced str is: \(result)")
+
+
+func ISVowel(char: Character)->Bool {
+    let vowels = "AEIOUaeiou"
+    return vowels.contains(char)
+}
