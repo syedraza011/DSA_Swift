93944ee839da801f5839e01bfc34aee8f2545c97
Syed Raza <syed.raza011@gmail.com>
2023-08-17 16:18:08 -0400

    added some new questions to be solved

---
+++ Swift/DSA.playground/Pages/Palindrome.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// using String **Palindrome Check:**
+//Create a function that checks if a given string is a palindrome (reads the same forwards and backwards).
---
+++ Swift/DSA.playground/Pages/Reverse a string.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Write a function to reverse a given string.
+//For example, "hello" should become "olleh""
+
+
