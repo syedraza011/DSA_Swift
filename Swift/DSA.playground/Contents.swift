15c11710ab91634a59279c05618b18c7e1c875df
Syed Raza <syed.raza011@gmail.com>
2023-08-17 15:46:55 -0400

    added sorting a string & converting chars array in to a single word of string

--- Swift/DSA.playground/Contents.swift
+++ Swift/DSA.playground/Contents.swift
@@ -8,7 +8,16 @@ var greeting = "Hello, playground"
 //remove number from it
 //remove chars from it
 //use of array
-let str: String = "Hello5"
+let str = "zmHello5t"
+
+let num = str.contains(where: {$0.isNumber})
+print(num)
+
+if let lastChar = str.last, lastChar.isNumber {
+    print("it is a number ")
+}else {
+    print("it is a not number ")
+}
 //access only number
 for (index, char) in str.enumerated() {
     if char.isNumber
@@ -19,3 +28,9 @@ for (index, char) in str.enumerated() {
     }
 }
 ////sort these
+let sorted = str.sorted {  (char1 , char2) in
+    return char1 < char2
+    
+}
+let sortedStr = String(sorted)
+print(sortedStr)
