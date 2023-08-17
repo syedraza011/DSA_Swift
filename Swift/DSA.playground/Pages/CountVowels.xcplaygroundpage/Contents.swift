55f065881a5b827040eea584010b2063838939eb
Syed Raza <syed.raza011@gmail.com>
2023-08-17 19:23:14 -0400

    Count Vowels & Consonants done!

--- Swift/DSA.playground/Pages/CountVowels.xcplaygroundpage/Contents.swift
+++ Swift/DSA.playground/Pages/CountVowels.xcplaygroundpage/Contents.swift
@@ -1,7 +1,28 @@
-//: [Previous](@previous)
 
 import Foundation
 
-// **Count Vowels and Consonants:** Write a program to count the number of vowels and consonants in a given string.
+// **Count Vowels and Consonants:**
+//Write a program to count the number of vowels and consonants in a given string.
 //
-//
+let str = "Hello"
+let arr = str.split(separator: "")
+print(arr)
+var vowe = 0
+var alpha = 0
+for chara in str {
+    
+    if isVowel(char: chara) {
+        vowe += 1
+    }else {
+        alpha += 1
+    }
+
+}
+print("Amount of Vowels is: \(vowe)")
+print("Amount of Consonents is: \(alpha)")
+func isVowel(char: Character)-> Bool{
+        let vowels = "aeiou"
+            return vowels.contains(char)
+        }
+    
+// 
---
+++ Swift/DSA.playground/Pages/RemoveDuplicates.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//using String **Remove Duplicates:**
+//Create a function that removes duplicate characters from a string.
+
+
