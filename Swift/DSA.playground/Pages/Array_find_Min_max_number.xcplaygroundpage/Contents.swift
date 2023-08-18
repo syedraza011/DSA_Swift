d932a8e88b3d9373aa69175be22b8cbdabbf4da7
Syed Raza <syed.raza011@gmail.com>
2023-08-17 20:52:43 -0400

    manually finding a way of doing Min & Max of an Array

--- Swift/DSA.playground/Pages/Array_find_Min_max_number.xcplaygroundpage/Contents.swift
+++ Swift/DSA.playground/Pages/Array_find_Min_max_number.xcplaygroundpage/Contents.swift
@@ -15,4 +15,19 @@ print("smallest :\(newArr[0])")
 print("Largest :\(newArr[newArr.count-1])")
 
 //other way of finding smallest and largest
+let myArr = [30,50,79,80,85,10000,9]
 
+var small = myArr[0]
+var large = myArr[0]
+for i in 0..<myArr.count {
+    if myArr[i] < small
+        {
+            small = myArr[i]
+        }
+    if myArr[i] > large
+        {
+            large = myArr[i]
+        }
+}
+print("smallest of small :\(small)")
+print("Largest of large :\(large)")
