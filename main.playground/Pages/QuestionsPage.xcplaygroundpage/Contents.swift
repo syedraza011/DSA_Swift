04cf93117886471a45f1b19ceda6067f21186cd0
Syed Raza <syed.raza011@gmail.com>
2023-08-18 14:48:55 -0400

    using dictionary count char's occurrence in the string

--- main.playground/Pages/QuestionsPage.xcplaygroundpage/Contents.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-
-//3. Create a function to count the occurrences of a specific character in a string.
-
---
+++ main.playground/Pages/countOccurrencesOfChar.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+//3. Create a function to count the occurrences of a specific character in a string.
+//let str = "HelloiamO"
+////using dictinary
+
+let str = "Hello0"
+
+var dic: [String: Int] = [:] // Specify the type explicitly for better clarity
+for char in str {
+    print(dic[String(char)] )
+    if let count = dic[String(char)] {
+        dic[String(char)] = count + 1
+     
+    } else {
+        dic[String(char)] = 1
+    }
+}
+print(dic)
